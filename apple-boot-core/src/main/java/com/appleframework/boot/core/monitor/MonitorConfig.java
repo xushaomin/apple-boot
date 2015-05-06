7b521c1cb5cad546b9709111df5245eaba57fe69
xushaomin <xushaomin@foxmail.com>
2015-05-06 16:18:03 +0800

    v0.1.5 新增MonitorContainer

--- apple-boot-core/src/main/java/com/appleframework/boot/core/monitor/MonitorConfig.java
+++ apple-boot-core/src/main/java/com/appleframework/boot/core/monitor/MonitorConfig.java
@@ -12,15 +12,18 @@ public class MonitorConfig implements MonitorConfigMBean {
 	// private Integer webPort;
 	// private Integer jmxPort;
 	// private String installPath;
+	// private String protocolName;
 
 	private static String KEY_APPLICATION_NAME = "application.name";
-	private static String KEY_DATA_ID = "deploy.dataId";
-	private static String KEY_GROUP = "deploy.group";
-	private static String KEY_ENV = "deploy.env";
-	private static String KEY_SERVICE_PORT = "service.port";
-	private static String KEY_WEB_PORT = "web.port";
-	private static String KEY_JMX_PORT = "jmx.port";
-	private static String KEY_INSTALL_PATH = "install.path";
+	private static String KEY_DATA_ID          = "deploy.dataId";
+	private static String KEY_GROUP            = "deploy.group";
+	private static String KEY_ENV              = "deploy.env";
+	private static String KEY_SERVICE_PORT     = "service.port";
+	private static String KEY_WEB_PORT         = "web.port";
+	private static String KEY_JMX_PORT         = "jmx.port";
+	private static String KEY_INSTALL_PATH     = "install.path";
+	private static String KEY_PROTOCOL_NAME    = "protocol.name";
+	
 
 	public String getApplicationName() {
 		return SystemPropertiesUtils.getString(KEY_APPLICATION_NAME);
@@ -53,5 +56,11 @@ public class MonitorConfig implements MonitorConfigMBean {
 	public String getInstallPath() {
 		return SystemPropertiesUtils.getString(KEY_INSTALL_PATH);
 	}
+
+	public String getProtocolName() {
+		return SystemPropertiesUtils.getString(KEY_PROTOCOL_NAME);
+	}
+	
+	
 
 }
--- apple-boot-core/src/main/java/com/appleframework/boot/core/monitor/MonitorConfigMBean.java
+++ apple-boot-core/src/main/java/com/appleframework/boot/core/monitor/MonitorConfigMBean.java
@@ -18,4 +18,6 @@ public interface MonitorConfigMBean {
 
 	public String getInstallPath();
 	
+	public String getProtocolName();
+	
 }
