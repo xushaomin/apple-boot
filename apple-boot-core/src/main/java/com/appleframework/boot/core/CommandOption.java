cd4663dc2b647578211b655089b2b72be7b76282
xushaomin <xushaomin@foxmail.com>
2016-07-12 23:45:07 +0800

    将env设到system.property

---
+++ apple-boot-core/src/main/java/com/appleframework/boot/core/CommandOption.java
@@ -0,0 +1,25 @@
+package com.appleframework.boot.core;
+
+import org.apache.log4j.Logger;
+
+public class CommandOption {
+
+	private static Logger logger = Logger.getLogger(CommandOption.class);
+
+	public static void parser(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			String envArgs = args[i];
+			String[] envs = envArgs.split("=");
+			if (envs.length == 2) {
+				String key = envs[0];
+				String value = envs[1];
+				System.setProperty(key, value);
+				logger.warn("配置项：" + key + "=" + value);
+
+			} else {
+				logger.error("错误参数：" + envArgs);
+			}
+		}
+	}
+
+}
