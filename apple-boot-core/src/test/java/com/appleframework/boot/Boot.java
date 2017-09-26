4127a2cb9f5623c42095d20c19444e7b8bbc824d
xushaomin <xushaomin@foxmail.com>
2017-09-26 14:39:57 +0800

    update

---
+++ apple-boot-core/src/test/java/com/appleframework/boot/Boot.java
@@ -0,0 +1,11 @@
+package com.appleframework.boot;
+
+import com.appleframework.boot.core.ContainerInit;
+
+public class Boot {
+
+	public static void main(String[] args) {
+		ContainerInit.initMonitorContainer();
+	}
+
+}
