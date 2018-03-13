f58da655d302c0ab46cac5a2440cb6d7b9905317
Cruise.Xu <xushaomin@foxmail.com>
2018-03-13 17:19:43 +0800

    0.3.4.RELEASE 修改slf4j日志支持

--- apple-boot-core/src/main/java/com/appleframework/boot/core/logging/LoggingContainer.java
+++ apple-boot-core/src/main/java/com/appleframework/boot/core/logging/LoggingContainer.java
@@ -2,7 +2,9 @@ package com.appleframework.boot.core.logging;
 
 import com.appleframework.boot.core.Container;
 import com.appleframework.boot.core.logging.log4j.Log4jContainer;
+import com.appleframework.boot.core.logging.log4j.Log4jUtils;
 import com.appleframework.boot.core.logging.logback.LogbackContainer;
+import com.appleframework.boot.core.logging.logback.LogbackUtils;
 
 /**
  * LoggingContainer
@@ -10,8 +12,11 @@ import com.appleframework.boot.core.logging.logback.LogbackContainer;
  * @author cruise.xu
  */
 public class LoggingContainer {
+	
+	private static String LOG_CONTAINER = "log4j";
 
 	public static Container getLoggingContainer(String logContainer) {
+		LOG_CONTAINER = logContainer;
 		if (null == logContainer) {
 			return new Log4jContainer();
 		} else if (logContainer.equals("log4j")) {
@@ -22,6 +27,21 @@ public class LoggingContainer {
 			return new LogbackContainer();
 		} else {
 			return new Log4jContainer();
+		}
+	}
+	
+	public static String getRootLoggerLevelString() {
+		if("log4j".equals(LOG_CONTAINER)) {
+			return Log4jUtils.getRootLoggerLevelString();
+		}
+		else if("slf4j".equals(LOG_CONTAINER)) {
+			return LogbackUtils.getRootLoggerLevelString();
+		}
+		else if("logback".equals(LOG_CONTAINER)) {
+			return LogbackUtils.getRootLoggerLevelString();
+		}
+		else {
+			return null;
 		}
 	}
 }
--- apple-boot-core/src/main/java/com/appleframework/boot/core/logging/logback/LogbackUtils.java
+++ apple-boot-core/src/main/java/com/appleframework/boot/core/logging/logback/LogbackUtils.java
@@ -2,12 +2,19 @@ package com.appleframework.boot.core.logging.logback;
 
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 
 public class LogbackUtils {
 	
 	public static LoggerContext getLoggerContext() {
         return (LoggerContext) LoggerFactory.getILoggerFactory();
+	}
+	
+	public static String getRootLoggerLevelString() {
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+		Logger logger = loggerContext.getLogger("root");  
+		return logger.getLevel().levelStr;
 	}
 
 }
